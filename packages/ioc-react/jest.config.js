0ce369da2837110c7c9e0897b2a1c24f16677221
Igor Vorobiov <igor.vorobioff@gmail.com>
2025-06-02 22:16:51 +0300

    fixes tests

--- packages/ioc-react/jest.config.js
+++ packages/ioc-react/jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  ...require('../../jest.config.base')
+  ...require('../../jest.config.base'),
+  testEnvironment: 'jsdom'
 };
--- packages/ioc-react/tests/useService.test.tsx
+++ packages/ioc-react/tests/useService.test.tsx
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom';
+import { Container } from '@ivorobioff/ioc-container';
+import { ServiceContext, useService } from '../src';
+
+function MyText() {
+  const text = useService('my-text');
+  return <>{text}</>;
+}
 
 describe('useService', () => {
-  it('checks', () => {
-    render(<div>foo</div>);
+
+  let container: Container;
+
+  beforeEach(() => {
+    container = new Container();
+  });
+
+  it('renders the text provided by the useService hook ', () => {
+
+    container.registerInstance('my-text', 'apple is not pear');
+
+    render(<ServiceContext.Provider value={container}><MyText /></ServiceContext.Provider>);
+
+    expect(screen.getByText('apple is not pear')).toBeTruthy();
   });
 });
