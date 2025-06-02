e4ab879a4ad4a27c0e63cb116a91614796a98cf0
Igor Vorobiov <igor.vorobioff@gmail.com>
2025-06-02 14:04:13 +0300

    some updates

---
+++ packages/ioc-react/tests/useService.test.tsx
@@ -0,0 +1,9 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom';
+
+describe('useService', () => {
+  it('checks', () => {
+    render(<div>foo</div>);
+  });
+});
